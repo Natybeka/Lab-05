958971305c3a923abf6028c89d154862115fd8e6
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2021-01-31 14:01:34 +0300

    BOM objects explored

--- BOMObject/assets/js/bom.js
+++ BOMObject/assets/js/bom.js
@@ -18,13 +18,46 @@ Good Luck !!!
 
 
 // Define UI Variables  here 
+//Location
+const href = document.querySelector('#url');
+const protocol = document.querySelector('#protocol');
+const host = document.querySelector('#host');
+const port = document.querySelector('#port');
+const hostName = document.querySelector('#host-name');
 
+//Browser Information
+const appName = document.querySelector('#app-name');
+const appVersion = document.querySelector('#app-version');
+const platform = document.querySelector('#platform');
+const language = document.querySelector('#language');
+const cookieEnabled = document.querySelector('#cookie-enabled');
 
+//Screen Information
+const height = document.querySelector('#height');
+const width = document.querySelector('#width');
+const pixelDepth = document.querySelector('#pixel-depth');
 
-
-
-
-
+//Browsing History Information
+const length = document.querySelector('#length');
+const state = document.querySelector('#state');
 
 
 // Display the BOM Information on the innerHTML of the elements
+href.innerHTML = location.href;
+protocol.innerHTML = location.protocol;
+host.innerHTML = location.host;
+port.innerHTML = location.port;
+hostName.innerHTML = location.hostname;
+
+appName.innerHTML = navigator.appName;
+appVersion.innerHTML = navigator.appVersion;
+platform.innerHTML = navigator.platform;
+language.innerHTML = navigator.language;
+cookieEnabled.innerHTML = navigator.cookieEnabled;
+
+height.innerHTML = screen.height;
+width.innerHTML = screen.width;
+pixelDepth.innerHTML = screen.pixelDepth;
+
+length.innerHTML = history.length;
+state.innerHTML = history.state;
